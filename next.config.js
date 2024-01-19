7aa4b28666f41af27374cfed25db20abb6f6d61d
Adib <155450377+AdibAcoup@users.noreply.github.com>
2024-01-19 16:09:30 +0400

    13commit

--- next.config.js
+++ next.config.js
@@ -1,13 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  remotePatterns: [
-    {
-      protocol: "https",
-      hostname: "**",
-      port: "",
-      pathname: "**",
-    },
-  ],
+  reactStrictMode: true,
+  i18n,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/styles")],
+    prependData: `@import "variables.scss";`,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
 };
-
 module.exports = nextConfig;
