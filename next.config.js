d693546f403707c26d753f6e3b61e85d1f2e74ed
Adib <155450377+AdibAcoup@users.noreply.github.com>
2024-01-19 16:03:31 +0400

    12commit

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   remotePatterns: [
     {
       protocol: "https",
-      hostname: "oaidalleapiprodscus.blob.core.windows.net",
+      hostname: "**",
       port: "",
       pathname: "**",
     },
